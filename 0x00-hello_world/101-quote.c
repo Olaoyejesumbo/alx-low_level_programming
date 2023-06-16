189af8db937949d3647a5f6be45a3232a417d646
Olaoyejesumbo <jesumbopeace@gmail.com>
2023-06-16 09:29:40 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
 *main - entry point
-*Return: 1 (success)
+*Return: 1 (error)
 */
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n", stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
 

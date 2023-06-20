16abc8a09b7170415cf009c39c9bdb963d0f3859
Olaoyejesumbo <jesumbopeace@gmail.com>
2023-06-20 12:25:14 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+
+putchar('-');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+
+return (0);
+}
+
+
